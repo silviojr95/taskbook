0b042be575f59ad5bd65de9ff239f405a76c6c52
Silvio Junior <silvio_jr95@hotmail.com>
2018-12-02 11:03:44 -0200

    Add files via upload

---
+++ help.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = `
+  Usage
+    $ tb [<options> ...]
+
+    Options
+        none             Display board view
+      --task, -t         Create task
+      --note, -n         Create note
+      --timeline, -i     Display timeline view
+      --delete, -d       Delete item
+      --check, -c        Check/uncheck task
+      --star, -s         Star/unstar item
+      --copy, -y         Copy item description
+      --list, -l         List items by attributes
+      --find, -f         Search for items
+      --edit, -e         Edit item description
+      --move, -m         Move item between boards
+      --priority, -p     Update priority of task
+      --archive, -a      Display archived items
+      --restore, -r      Restore items from archive
+      --clear            Delete all checked items
+      --help, -h         Display help message
+      --version, -v      Display installed version
+      --font             Font color settings
+
+    Examples
+      $ tb
+      $ tb --task Make some buttercream
+      $ tb --task @coding Improve documentation
+      $ tb --task @coding @reviews Review PR #42
+      $ tb --note @coding Mergesort worse-case O(nlogn)
+      $ tb --check 1 2
+      $ tb --delete 4
+      $ tb --star 2
+      $ tb --copy 1 2 3
+      $ tb --priority @3 2
+      $ tb --timeline
+      $ tb --edit @3 Merge PR #42
+      $ tb --move @1 cooking
+      $ tb --find documentation
+      $ tb --list pending coding
+      $ tb --archive
+      $ tb --restore 4
+      $ tb --clear
+      $ tb --font
+`;
